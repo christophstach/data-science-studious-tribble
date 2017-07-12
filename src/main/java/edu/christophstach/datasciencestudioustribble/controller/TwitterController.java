85c1c5e39fb9efa56c9dac91af1978efea2a49ae
Christoph Stach <christoph.stach@gmail.com>
2017-07-12 20:39:18 +0200

    fixed timezone bug

--- src/main/java/edu/christophstach/datasciencestudioustribble/controller/TwitterController.java
+++ src/main/java/edu/christophstach/datasciencestudioustribble/controller/TwitterController.java
@@ -118,7 +118,7 @@ public class TwitterController {
   @GetMapping("/twitter/relevant-hash-tags-weekly")
   public List<HashTagResult> relevantHashTagsWeekly() {
     List<HashTagResult> list = new LinkedList<>();
-    Long days = ChronoUnit.DAYS.between(LocalDate.of(2017, Month.MAY, 18), LocalDate.now(ZoneId.of("EuropeBerlin")));
+    Long days = ChronoUnit.DAYS.between(LocalDate.of(2017, Month.MAY, 18), LocalDate.now());
 
     for (int i = 0; i <= days; i = i + 7) {
       LocalDate monday = LocalDate.now().minusDays(i);
